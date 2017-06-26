df073126bb62ccec283ca631cbd6250768f05e60
malaysat <malay.satapathy@invenio-solutions.com>
2017-06-26 16:25:52 +0530

    complete project

---
+++ src/main/java/com/message_exchange/setGetMessage.java
@@ -0,0 +1,22 @@
+package com.message_exchange;
+
+public class setGetMessage
+{
+	String name="vishal Good Morning";
+	
+
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+		this.name=name;
+		
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+
+
+}
---
+++ src/main/java/com/message_exchange/setGetName.java
@@ -0,0 +1,31 @@
+package com.message_exchange;
+
+import org.mule.api.lifecycle.Callable;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+
+public class setGetName implements Callable
+{
+	 
+	 String nmsg;
+
+	@Override
+	public String onCall(MuleEventContext eventContext) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		 String msg=  eventContext.getMessageAsString();
+		
+        StringBuilder sb= new StringBuilder(msg);
+		 
+		 sb.replace(0, 6, "Malay");
+		 
+		 nmsg=sb.toString();
+		 return nmsg;
+		
+	}
+
+	
+
+	
+}
